d5a91b9a9d315c5f0c3e306435d2d6c6f12cb8ae
Ananias Chorozoglou <65049135+Anomacti@users.noreply.github.com>
2024-05-31 23:50:45 +0300

    Delete rebuild.sh

--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker stop strm2
-docker build -t anomacti/strm1 .
-docker run -d --rm -p 8502:8501 -v /home/anomacti/Dev/University/software/data-analysis-web:/app --name strm2 anomacti/strm1
